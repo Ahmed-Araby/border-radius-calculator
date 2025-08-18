ad4e83cc10720edda4b501a66808815a3bb26d35
Ahmed Araby <ahmedaraby605@gmail.com>
2025-08-18 10:56:22 +0300

    change the rectangle dimensions according to user preference

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ console.log("init Border Radius Calculator App");
 const CSS_SELECTORS = {
     MEASUREMENT_UNIT_RADIO_BUTTONS: "input[name='unit'][type='radio']",
     ELLIPSE_SEMI_AXIS_UNIT: ".semi-axis-unit",
+    RECT_DIMENSION_WIDTH: "#rect-width-input",
+    RECT_DIMENSIONS_FORM: ".rect-dimensions-form",
+    RECT: "#rect",
 };
 
 class Rectangle {
@@ -22,7 +25,7 @@ class Ellipse {
 
 appState = {
     rect: new Rectangle(100, 100),
-    measurementUnit: "Pixels",
+    measurementUnit: "px",
     topLeftEllipse: new Ellipse(0, 0),
     topRightEllipse: new Ellipse(0, 0),
     bottomRightEllipse: new Ellipse(0, 0),
@@ -33,16 +36,37 @@ document.addEventListener('DOMContentLoaded', () => {
     console.log("DOM fully loaded and parsed");
     console.log("register event listeners");
     listenOnMeasurementUnitRadioButtonsSelection();
+    listenOnRectDimensionsChange()
 });
 
 
 function listenOnMeasurementUnitRadioButtonsSelection() {
     document.querySelectorAll(CSS_SELECTORS.MEASUREMENT_UNIT_RADIO_BUTTONS).forEach((radioButton) => {
         radioButton.addEventListener('change', (event) => {
-            console.log("event = ", event)
+            console.log("MEasurement Unit Radio Button Selection event = ", event)
             handleMeasurementUnitSelection(event);
         });
     });
+}
+
+function listenOnRectDimensionsChange() {
+    document.querySelector(CSS_SELECTORS.RECT_DIMENSIONS_FORM).addEventListener('submit', (event) => {
+        console.log("Rectangle Dimensions Fomr Submitation event = ", event);
+        event.preventDefault();
+
+        const formData = new FormData(event.target);
+        setRectDimensions(formData.get("width"), formData.get("height"));
+    });
+}
+
+function setRectDimensions(width, height) {
+    const rectHtmlElem = document.querySelector(CSS_SELECTORS.RECT);
+    rectHtmlElem.style.width = width + "px";
+    rectHtmlElem.style.height = height + "px";
+}
+
+function setEllipsesDimensions(parentRect) {
+
 }
 
 function handleMeasurementUnitSelection(event) {
