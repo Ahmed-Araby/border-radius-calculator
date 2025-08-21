fef349f0902600f1d4f486c405bce9ce06d88479
Ahmed Araby <ahmedaraby605@gmail.com>
2025-08-21 16:19:11 +0300

    Fix Bug: only reduce the Canvas's Rectangle size when the there is no space to fit the current rectangle

--- app/listeners/WindowEventsListener.js
+++ app/listeners/WindowEventsListener.js
@@ -2,6 +2,7 @@ import { RectDimensionInput } from "../knobs/RectDimensionInput.js";
 import { CSSSelectors } from "../CSSSelectors.js";
 import { MathUtil } from "../utils/MathUtil.js";
 import { CanvasRectangle } from "../canvas/CanvasRectangle.js";
+import { AppContext } from "../AppContext.js";
 
 export class WindowEventListenrs {
 
@@ -18,7 +19,9 @@ export class WindowEventListenrs {
             const rectHeight = MathUtil.restrictDecimalDigits(canvasH * 0.8, 3);
             
             RectDimensionInput.setDimensionsMaxAndClampValue(rectWidth, rectHeight); 
-            RectDimensionInput.handleRectDimensionsInputChange(rectWidth, rectHeight);
+            if(AppContext.rect.getWidth() > rectWidth || AppContext.rect.getHeight() > rectHeight) {
+                RectDimensionInput.handleRectDimensionsInputChange(Math.min(rectWidth, AppContext.rect.getWidth()), Math.min(rectHeight, AppContext.rect.getHeight()));
+            }
             CanvasRectangle.clampDimensions(rectWidth, rectHeight);
         });
     }
